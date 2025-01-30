7bbead33b3e027e415f8ad99f2cd2686b3c1d4ff
Deamien <snejko96@gmail.com>
2025-01-30 20:58:38 +0100

    Admin logiken kommer snart

---
+++ LAB1_HT2024/Models/AdminLogin.cs
@@ -0,0 +1,11 @@
+﻿namespace LAB1_HT2024.Models
+{
+    public class AdminLogin
+    {
+        public int id { get; set; }
+
+        public string Username { get; set; }
+
+        public string Passwordhash { get; set; }
+    }
+}
